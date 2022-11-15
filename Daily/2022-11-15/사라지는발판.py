e447a05afe46a922b1aca6fa4010ac6e7e243e0b
Jeffrey <jsrimr@naver.com>
2022-11-15 15:14:58 +0900

    roll back 할 때 한쪽 플레이어만 rollback 하면 되는게 아닌가/

--- Daily/2022-11-15/사라지는발판.py
+++ Daily/2022-11-15/사라지는발판.py
@@ -3,11 +3,11 @@ def solution(board, aloc, bloc):
 
     R = len(board)
     C = len(board[0])
-    # a : 0 , b : 1
-    loc = [aloc, bloc]
+    loc = [aloc, bloc]  # a : 0 , b : 1
 
     # 누가 이길 지 부터 알아내자
-    def move(player):  # player 가 이길 것인가? -> True/False
+    def move(player, step, hist):  # player 가 이길 것인가? -> True/False
+        print(hist)
         x, y = loc[player]
         if board[x][y] == 0:  # 발판 사라졌으면 짐
             return False
@@ -15,19 +15,20 @@ def solution(board, aloc, bloc):
         # 갈 데 있는 지 체크
         for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < R and 0 <= ny < C:
-                if board[nx][ny] == 1:
-                    loc[player] = (nx, ny)
-                    board[x][y] = 0
-                    if move(1 if player == 0 else 0):  # 내가 승리 못할거라면 이 길 가면 안됨.
-                        board[x][y] = 1
-                        loc[player] = (x, y)
-                    else:
-                        return True
-        else:
-            return False
+            if 0 <= nx < R and 0 <= ny < C and board[nx][ny] == 1:
+                loc[player] = (nx, ny)
+                board[x][y] = 0
+                hist['a'].append((nx, ny)) if player == 0 else hist['b'].append((nx, ny))
+                if move(1 if player == 0 else 0, step + 1, hist):  # 내가 승리 못할거라면 이 길 가면 안됨; 최대한 시간 끌기
+                    print(f'{"a" if player == 0 else "b"} dont want to lose. rollback!')
+                    board[x][y] = 1
+                    loc[player] = (x, y)
+                    hist['a'].pop() if player == 0 else hist['b'].pop()
+                else:  # 내가 승리할 수 있다면 이 길 가야함; 최대한 빨리 끝내기
+                    return True
 
-    winner = move(0)
+
+    winner = move(0, 0, dict(a=[aloc], b=[bloc]))
     print(winner)
 
     # 이기는 쪽은 min step 하도록, 지는 쪽은 max step 하도록 움직이자
